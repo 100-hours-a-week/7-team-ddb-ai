d86b252e7b8dcf9323b21bc95a00708774bc0550
chanhue <hwagchan@gmail.com>
2025-07-13 20:49:08 +0900

    feat : hyperclovax basemodel 로드 구현
    - peft 라이브러리 도입
    - basemodel 로딩 코드 추가
     #215

---
+++ fastapi_app/app/services/clovax_factory.py
@@ -0,0 +1,31 @@
+import threading
+from transformers import AutoTokenizer, pipeline, AutoModelForCausalLM
+from langchain_community.llms import HuggingFacePipeline
+from app.core.config import settings
+from peft import PeftModel
+
+class ClovaXFactory:
+    _instance = None
+    _lock = threading.Lock()
+
+    @classmethod
+    def _create_instance(cls):
+        model_id = settings.CLOVAX_MODEL_NAME  # 예: "chanhue/dolpin-hyperclova-lora"
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        base_model = AutoModelForCausalLM.from_pretrained("naver-hyperclovax/HyperCLOVAX-SEED-Text-Instruct-1.5B")
+        model = PeftModel.from_pretrained(base_model, model_id)
+        hf_pipe = pipeline(
+            "text-generation",
+            model=model,
+            tokenizer=tokenizer,
+            max_new_tokens=256
+        )
+        return HuggingFacePipeline(pipeline=hf_pipe)
+
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            with cls._lock:
+                if cls._instance is None:
+                    cls._instance = cls._create_instance()
+        return cls._instance 
